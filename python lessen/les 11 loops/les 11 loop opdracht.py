0fe8016645a3a8b55887ec0109eb146dbc69697d
tim10997 <timvdbossche99@gmail.com>
2025-03-28 14:34:51 +0100

    Update les 11 loop opdracht.py

--- python lessen/les 11 loops/les 11 loop opdracht.py
+++ python lessen/les 11 loops/les 11 loop opdracht.py
@@ -4,8 +4,10 @@ import random
 
 aantal_worpen = 0
 totaal_van_de_worpen = 0
-while aantal_worpen != 0:
+while aantal_worpen < 10:
     eerste_steen = random.randint(1, 6)
     tweede_steen = random.randint(1, 6)
     dobbelsteen_som = eerste_steen + tweede_steen
     print("Je rolde een " + str(dobbelsteen_som) + "!")
+    aantal_worpen = aantal_worpen + 1
+    

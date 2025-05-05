d1297fc5029ee7504bdc0a9f25316560e98d3204
tim10997 <timvdbossche99@gmail.com>
2025-05-05 13:08:21 +0000

    les 11 opdracht gemaakt

--- python lessen/les 11 loops/les 11 loop opdracht.py
+++ python lessen/les 11 loops/les 11 loop opdracht.py
@@ -4,10 +4,15 @@ import random
 
 aantal_worpen = 0
 totaal_van_de_worpen = 0
-while aantal_worpen < 10:
+lopende_totaal = 0
+hoeveel_worpen = int(iput("hoeveel keer wil je het expiriment uitvoeren"))
+while aantal_worpen < hoeveel_worpen:
     eerste_steen = random.randint(1, 6)
     tweede_steen = random.randint(1, 6)
     dobbelsteen_som = eerste_steen + tweede_steen
     print("Je rolde een " + str(dobbelsteen_som) + "!")
     aantal_worpen = aantal_worpen + 1
-    
+    lopende_totaal = lopende_totaal + dobbelsteen_som
+
+gem_waarde = lopende_totaal / 10
+print("het gemiddelde van de 10 worpen is " + str(gem_waarde))
